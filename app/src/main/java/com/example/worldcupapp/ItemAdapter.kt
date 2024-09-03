7b07722450898280a1d95f6829f284ec0a69ec2e
Kolchin13D <kolchin13d@gmail.com>
2024-09-03 12:12:18 +0300

    testing

---
+++ app/src/main/java/com/example/worldcupapp/ItemAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.worldcupapp
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class ItemAdapter(private var activity: Activity, private var items: ArrayList<CountryModel>) :
+    BaseAdapter() {
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun getItem(position: Int): CountryModel {
+        return items[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View?
+        val viewHolder: ViewHolder
+
+        if (convertView == null) {
+            val inflater =
+                activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = inflater.inflate(R.layout.list_item, null)
+            viewHolder = ViewHolder(view)
+            view?.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
+        var countries = items[position]
+        viewHolder.textName?.text = countries.name
+        viewHolder.textWins?.text = countries.wins
+        viewHolder.image?.setImageResource(countries.flagImg)
+
+        view?.setOnClickListener(){
+
+        }
+
+        return view as View
+    }
+
+    private class ViewHolder(row: View?) {
+        var textName: TextView? = null
+        var textWins: TextView? = null
+        var image: ImageView? = null
+
+        init {
+            this.textName = row?.findViewById(R.id.nameCountry)
+            this.textWins = row?.findViewById(R.id.winCount)
+            this.image = row?.findViewById(R.id.flagImage)
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/worldcupapp/MainActivity.kt
+++ app/src/main/java/com/example/worldcupapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.worldcupapp
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,20 +25,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         // adapter view
-        var listview: ListView = findViewById(R.id.listView)
+        //var listview: ListView = findViewById(R.id.listView)
+        var listview = findViewById<ListView>(R.id.listView)
 
-        //  data source
-        var countries = arrayOf("Russia", "Germany", "England", "Spain")
+        // old adapter
+//        val arrayAdapter: ArrayAdapter<*> = ArrayAdapter(
+//            this,
+//            R.layout.list_item,
+//            R.id.nameCountry,
+//            countries
+//        )
 
-        // adapter
-        val arrayAdapter: ArrayAdapter<*> = ArrayAdapter(
-            this,
-            R.layout.list_item,
-            R.id.nameCountry,
-            countries
-        )
+        //  new adapter
+        var adapter = ItemAdapter(this, generateData())
+        listview?.adapter = adapter
+        adapter?.notifyDataSetChanged()
 
-        listview.adapter = arrayAdapter
+    }
 
+    //  data source
+    //var countries = arrayOf("Russia", "Germany", "England", "Spain")
+    fun generateData(): ArrayList<CountryModel> {
+        var result = ArrayList<CountryModel>()
+        var country1: CountryModel = CountryModel("Russia", "5 wins", R.drawable.russia)
+        var country2: CountryModel = CountryModel("Germany", "1 wins", R.drawable.germany)
+        var country3: CountryModel = CountryModel("England", "3 wins", R.drawable.uk)
+        var country4: CountryModel = CountryModel("Spain", "2 wins", R.drawable.spain)
+
+        result.add(country1)
+        result.add(country2)
+        result.add(country3)
+        result.add(country4)
+
+        return result
     }
 }
